1fa2590bd67f7e96ce87e8e4102e4c1baa13bf66
Samuli Ahonen <samuli.ahonen@qvantel.com>
2018-02-14 16:49:50 +0200

    auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
+
+// 1016355779589-lcnu8vcu6cgs2r80e094heugkt7oemqa.apps.googleusercontent.com
+// FeHV5mcGtQ1AvYJGyYOcG2pP
+
