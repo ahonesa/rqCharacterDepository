fabdb46b0067f66ece37318ea616f63df91d9b26
Samuli Ahonen <samuli.ahonen@qvantel.com>
2018-03-03 12:07:31 +0200

    use initialValues

--- client/src/components/user-form/UserForm.js
+++ client/src/components/user-form/UserForm.js
@@ -1,24 +1,25 @@
+import _ from 'lodash';
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import TextField from '../fields/TextField';
 import { Link } from 'react-router-dom';
 import * as actions from "../../actions";
 import { connect } from "react-redux";
+import { loadavg } from "os";
 
 class UserForm extends Component {
+
   render() {
     const onSubmit = (values) => {
-      console.log(values)
       this.props.updateUser(values.name)
     };
-    console.log(this.props)
     const { handleSubmit, reset } = this.props
     return (
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row">
             <div className="card-panel teal lighten-5 col s12">
-              <Field key="name" component={TextField} type="text" label="User Name" name="name" />
+              <Field key="name" component={TextField} type="text" label="User Name" name="userName" />
               <Link to="/" className="red btn-flat left white-text" onClick={reset}>
                 Cancel
               </Link>
@@ -34,6 +35,10 @@ class UserForm extends Component {
   }
 }
 
-export default connect(null, actions)(reduxForm({
+function mapStateToProps({auth}) {
+  return { initialValues: auth }
+}
+
+export default connect(mapStateToProps, actions)(reduxForm({
   form: "userForm"
 })(UserForm));
